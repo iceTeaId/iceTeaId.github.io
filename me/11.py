537afa4f75bade227e00c71153c791d48dd6ac2e
iceTeaId <judinsiro90@gmail.com>
2020-06-11 23:18:29 +0700

    termux

--- me/11.py
+++
@@ -1,8 +0,0 @@
-import json
-import re
-import urllib2
-
-text = urllib2.urlopen('http://dcsd.nutrislice.com/menu/meadow-view/lunch/').read()
-menu = json.loads(re.search(r"bootstrapData\['menuMonthWeeks'\]\s*=\s*(.*);", text).group(1))
-
-print menu
