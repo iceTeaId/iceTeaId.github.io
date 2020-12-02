866a2627b2c74af5a75d5c9384213d21a5a1ad6b
iceTeaId <judinsiro90@gmail.com>
2020-12-02 14:28:08 +0700

    'Termux auto'

--- me/kstcp.py
+++ me/kstcp.py
@@ -1,30 +1,23 @@
-import socket,binascii
+import socket
 
 Host = 'ec2-54-151-177-35.ap-southeast-1.compute.amazonaws.com'
 
-def bytes(gg):
-	r=binascii.hexlify(gg.encode('utf-8'))#hex ke byte
-	return r
-
-def hexs(gg):
-	u=binascii.unhexlify(gg).decode('utf-8')#byte ke hex
-	return u
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((Host, 19129))
 #s.connect((socket.gethostname(),12346))
+print('connect')
 while True:
 	full_msg = ''
-	inf=input('Input :')
-	inp=bytes(inf)
+    #inp=codec.encode(inp)
+    #inp=bytes(input('input :'),'utf-8')
 	while True:
-		s.sendall(inp)
+    	#s.sendall(inp)
 		msg = s.recv(8)
 		print('connect')
 		if len(msg) <= 0:
 			break
-		print(hexs(msg))
-		#full_msg += hexs(msg)
+		full_msg += msg.decode('utf-8')
     	
 
 	if len(full_msg) > 0:
